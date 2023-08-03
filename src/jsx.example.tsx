bd8b743200032c448ef5fb241bec821954740ab3
Oleg Sobolev <heroh44444@gmail.com>
2023-08-03 12:10:48 +0300

    first commit

---
+++ src/jsx.example.tsx
@@ -0,0 +1,27 @@
+import React, {createElement as e, useState} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//     const [count, setCount] = useState(0)
+    // return (
+    //   <div className="App">
+    //     <h1>Hello React</h1>
+    //     <div className="test">
+    //         <button>test</button>
+    //     </div>
+    //   </div>
+    // );
+    // return e('div', {className: 'container'}, [
+    //     e('h1', {className: 'font-bold', key: 1}, `Test jsx ${count}`),
+    //     e('button', {
+    //         className: 'py-2 px-4 border',
+    //         key: 2,
+    //         onClick: () => {
+    //             setCount(count + 1)
+    //         }
+    //     }, 'Click me!')
+    // ]);
+// }
+//
+// export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci atque commodi consequuntur deserunt dolores est exercitationem facere ipsum laborum neque nobis odio officiis optio pariatur quas reiciendis suscipit tempora vero, voluptatum! Aut consectetur dolorem eius est fugit id inventore labore magnam magni perferendis quibusdam quo, recusandae unde. Adipisci, temporibus.</div>
+    )
+}
